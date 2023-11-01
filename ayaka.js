166c2e14760e52377d66e75282140228246ceea7
EbelAlfie <davis.onde.wei@gmail.com>
2023-11-01 21:44:13 +0700

    init ayaka brain

--- ayaka.js
+++ ayaka.js
@@ -1,18 +1,21 @@
 class Ayaka {
-    constructor() {
-        //this.brain = new CharacterAi() ;
+    constructor(config) {
+        this.ayakaId = config.ayakaId ;
+        this.userId = config.userId ;
+        this.brain = new CharacterAi() ;
     }
 
     onReady(client) {
         console.log(`Logged in as ${client.user.tag}`) ;
         //log in to character ai
+        this.brain.bringToLive() ;
     }
 
     async reply(interaction) {
         //Reply based on character ai as brain
         console.log(`${interaction}`) ;
         if (interaction.author.bot) return 
-    await interaction.channel.send("Hello") ;
+        await interaction.channel.send("Hello") ;
     }
 }
 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {Client, GatewayIntentBits, Events} = require("discord.js") ;
 const Ayaka = require("./ayaka.js") ;
-const { ayakaDiscord } = require("./config/config.json") ;
+const { ayakaDiscord, ayakaBrain, userAccessToken } = 
+    require("./config/config.json") ;
 
 const client = new Client({ intents: [
     GatewayIntentBits.Guilds,
@@ -9,7 +10,7 @@ const client = new Client({ intents: [
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.DirectMessageTyping,
 ] }) 
-const ayaka = new Ayaka();
+const ayaka = new Ayaka(ayakaBrain, userAccessToken);
 
 client.once(Events.ClientReady, () => ayaka.onReady(client)) ;
 client.on(Events.MessageCreate, ayaka.reply) ;
