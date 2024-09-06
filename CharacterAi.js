8929fe0801811131c5c41e9a69462b141d6dbedb
EbelAlfie <davis.onde.wei@gmail.com>
2024-09-06 07:03:00 +0700

    try variable

--- CharacterAi.js
+++ CharacterAi.js
@@ -7,25 +7,33 @@ class Brain {
     }
 
     async #createBrowser() {
-        this.#puppeteer.launch({ headless: "new" }).then( browser =>
+        await this.#puppeteer.launch({ headless: "false" }).then( browser =>
             this.#browser = browser 
         ).catch(error => {
             console.log(`Failed creating browser: ${error}`)
         }) ;
     }
     
-    async goTo() {
-        if (this.#browser === null) await this.#createBrowser() ;
-        await this.#browser.newPage().then(page =>{
-            page.goto('https://beta.character.ai/').then(val => {
-                console.log(val) ;
-            }).catch(error => {
-                console.log(`Goto failed ${error}`) ;
-            });
+    async goTo() {  
+        if (this.#browser === null)  await this.#createBrowser() ;
+        
+        if (this.#browser === null) return 
+
+        const page = await this.#browser.newPage();
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0')
+        await page.setExtraHTTPHeaders({
+            'Accept-Language': 'en'
+           });
+        page.goto('https://character.ai/', {
+            waitUntil: "domcontentloaded",
+        }).then(result => {
+            console.log(result) ;
         });
+        //await page.pdf({ path: 'example.pdf' });
+        this.closeBrowser() ;
     }
 
-    async closeBrowser() {
+    closeBrowser() {
         this.#browser.close().then( value =>{ 
             console.log("success closing browser") ;
         }).catch(error => {
