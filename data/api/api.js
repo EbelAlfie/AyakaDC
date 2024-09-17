ab5d95c616f18bec85ac3d2a9c9c89b837c87570
Davis Inde Satya <davis.onde.wei@gmail.com>
2024-09-17 10:08:20 +0700

    rapihin kode

--- data/api/api.js
+++ data/api/api.js
@@ -1,3 +1,21 @@
-export function checkIn() {
-    
+
+export class Api {
+    _encryption = ""
+
+    constructor() {
+        this._initApi()
+    }
+
+    _initApi() {
+        this._encryption = process.env.MIHOYO_ENCRYPTION_KEY
+    }
+
+    checkIn() {
+
+    }
+
+    login() {
+        
+    }
+
 }
