649bb79b544475cccafb957cb54ebf1ec98fe769
EbelAlfie <davis.onde.wei@gmail.com>
2024-10-14 20:37:35 +0700

    aded selection for time picker

--- presentation/components/selection.js
+++ presentation/components/selection.js
@@ -1,12 +1,19 @@
-import { StringSelectMenuBuilder } from "discord.js";
+import { StringSelectMenuBuilder, StringSelectMenuOptionBuilder } from "discord.js";
+import { Times } from "../models/Times";
+
+const options = () => {
+	return Times.map(item => {
+		new StringSelectMenuOptionBuilder()
+			.setLabel(item.label)
+			.setDescription("O'clock")
+			.setValue(item.value)
+	})
+}
 
 export const TimeSpinner = () => {
-
-	
-
     const row = new StringSelectMenuBuilder()
 		.setCustomId("scheduleSelect")
 		.setPlaceholder("Select your schedule")
-		.addOptions();
+		.addOptions(options);
     return row
 }
